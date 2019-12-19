8317e0768da27aefbd60ba55b53837e9134e8254
Fion-P <fionpang97@gmail.com>
2019-12-19 14:54:25 -0800

    create axes and labels

---
+++ src/labels+axes.js
@@ -0,0 +1,45 @@
+import { g } from './chart-area';
+import { x, y, area, continentColor } from './scales';
+
+// ============================= Labels ===================================
+
+const xLabel = g.append("text")
+  .attr("y", height + 50)
+  .attr("x", width / 2)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("GDP Per Capita ($)");
+const yLabel = g.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", -40)
+  .attr("x", -170)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("Life Expectancy (Years)")
+export const timeLabel = g.append("text")
+  .attr("y", height - 10)
+  .attr("x", width - 40)
+  .attr("font-size", "40px")
+  .attr("opacity", "0.4")
+  .attr("text-anchor", "middle")
+  .text("1800");
+
+
+// ============================= Axes ===================================
+
+// X Axis
+export const xAxisCall = d3.axisBottom(x)
+  .tickValues([400, 4000, 40000])
+  .tickFormat(d3.format("$"));
+g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0, " + height + ")")
+  .call(xAxisCall);
+
+// Y Axis
+export const yAxisCall = d3.axisLeft(y)
+  .ticks(10)
+  .tickFormat(d => { return +d; });
+g.append("g")
+  .attr("class", "y-axis")
+  .call(yAxisCall);
--- src/labels--axes.js
+++
@@ -1,2 +0,0 @@
-import { g } from './chart-area';
-import { x, y, area, continentColor } from './scales';
