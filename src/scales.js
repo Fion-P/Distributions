620e466015868d10ba8d8179ea41c182c659a287
Fion-P <fionpang97@gmail.com>
2019-12-24 01:13:15 -0800

    add color scale

--- src/scales.js
+++ src/scales.js
@@ -20,8 +20,8 @@ export const area = d3.scaleLinear()
 // Color Scale
 export const continentColor = d3.scaleOrdinal().range(["#65001c", "#335152", "#B58254", "#678869"] );
 
-export const americaColor = d3.scaleOrdinal().range(["#65001c", "#335152", "#B58254", "#678869"] );
-export const africaColor = d3.scaleOrdinal(d3.schemeGreens[9]);
+export const americaColor = d3.scaleOrdinal().range(["rgb(49, 32, 17)", "rgb(85, 68, 52)", "rgb(131, 97, 66)", "rgb(153, 118, 85)", "rgb(195, 156, 119)"] );
+export const africaColor = d3.scaleOrdinal().range(["#172918", "#2e4d30", "#48724b", "#609063", "#81c585"]);
 
 export const europeColor = d3.scaleOrdinal().range(["#2a0c14", "#551929", "#792a3f", "#a84963", "#c07388"]);
 export const asiaColor = d3.scaleOrdinal().range(["#192525", "#406162", "#557c7e", "#4098a0", "#6fb7bd"]);
